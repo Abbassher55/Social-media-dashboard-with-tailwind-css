143c7c5600c14e0638d866d7f0bae906469c581d
Abbas Sher <57453785+Abbassher55@users.noreply.github.com>
2024-07-15 00:10:47 +0500

    docs folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./dist/*.{html,js}"],
+  content: ["./docs/*.{html,js}"],
   theme: {
     container: {
       center: true,
